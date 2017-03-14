288274e7c6883e36b34fcc36b8b82c5fd37764ba
EL MEDIANI <fatima-zahra.el-mediani@capgemini.com>
2017-03-14 15:02:29 +0000

    Editing pom.xml

--- src/main/java/com/classe/ProjectJerseyApplication.java
+++ src/main/java/com/classe/ProjectJerseyApplication.java
@@ -2,11 +2,22 @@ package com.classe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectJerseyApplication {
+public class ProjectJerseyApplication extends SpringBootServletInitializer { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectJerseyApplication.class, args);
 	}
+	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	   return application.sources(ProjectJerseyApplication.class);
+	}
+
+	 
+
 }
